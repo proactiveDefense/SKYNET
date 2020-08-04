63f34e7b22394e50e7fa98e8224d7e0571f8e63f
NdA994 <nda994@gmail.com>
2020-08-04 05:38:01 -0400

    add terraform vars add script Skynet add terraform formation for mongoDB

---
+++ terraform.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  profile	= "default"
+  region  = "us-east-1"
+}
+
+variable "public_key_path" { type = string }
+variable "private_key_path" { type = string }
+variable "ami" { default = "ami-06c8ff16263f3db59" }
+
+resource "aws_key_pair" "skynet_key" {
+  key_name   = "key"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "skynet_dash" {
+  ami		    = var.ami
+  instance_type	= "t2.micro"
+  key_name		= aws_key_pair.skynet_key.key_name
+
+
+  #creates ssh connection to consul servers
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file(var.private_key_path)
+    host     = aws_instance.skynet_dash.public_ip
+  }
+
+}
---
+++ variables.auto.tfvars
@@ -0,0 +1,2 @@
+public_key_path  = "~/.ssh/skynet.pub"
+private_key_path = "~/.ssh/skynet"

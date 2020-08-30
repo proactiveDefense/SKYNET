97e6921be056109caa82592503f9834aba4ba0fc
NdA994 <nda994@gmail.com>
2020-08-30 14:51:23 -0400

    add correction

--- script/startHeralding.sh
+++ script/startHeralding.sh
@@ -5,7 +5,7 @@ set -efux
 sudo cp /tmp/metricbeat.yml /etc/metricbeat/metricbeat.yml
 sudo service metricbeat start
 
-#sudo cp /tmp/filebeat.yml /etc/filebeat/filebeat.yml
+sudo cp /tmp/filebeat-heralding.yml /etc/filebeat/filebeat.yml
 
 sudo iptables -t nat -A PREROUTING -p tcp --dport 25 -j REDIRECT --to-port 2525
 sudo iptables -t nat -A PREROUTING -p tcp --dport 21 -j REDIRECT --to-port 2121

25be7e66260fe2bbd426d8fb3b5575390fb2b7c6
NdA994 <nda994@gmail.com>
2020-09-04 17:39:42 -0400

    enable metricbeat filebeat heralding

--- script/startHeralding.sh
+++ script/startHeralding.sh
@@ -4,9 +4,13 @@ set -efux
 
 sudo cp /tmp/metricbeat.yml /etc/metricbeat/metricbeat.yml
 sudo service metricbeat start
+sudo systemctl enable metricbeat
+
 
 sudo cp /tmp/filebeat-heralding.yml /etc/filebeat/filebeat.yml
 sudo service filebeat start
+sudo systemctl enable filebeat
+
 
 sudo iptables -t nat -A PREROUTING -p tcp --dport 25 -j REDIRECT --to-port 2525
 sudo iptables -t nat -A PREROUTING -p tcp --dport 21 -j REDIRECT --to-port 2121

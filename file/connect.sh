e2d9c7c30341bd45751524ba25042f5ecb3f4ea3
NdA994 <nda994@gmail.com>
2020-09-25 12:56:22 -0400

    add telegram bot interaction

---
+++ file/connect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d "{\"chat_id\": \"-1001409159111\", \"text\": \"IP connection: $1\", \"disable_notification\": true}" \
+     https://api.telegram.org/bot1380103530:AAHYF-i_JyA0_iV5NP2-JFNI3UYhQ3EJsCA/sendMessage
